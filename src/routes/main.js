154e60b332735bc37f30191e576ac3922eb5cc4b
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-09-14 02:58:29 +0500

    Update main.js

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const routes = express.Router();
 const Detail = require("../models/Detail");
+const Slider = require("../models/Slider");
 
 
 
@@ -8,17 +9,22 @@ routes.get("/", async (req, res) => {
  const details = await Detail.findOne({
     "_id":"632063b74fd8ba361252e4fc",
   });
-  console.log(details)
+  //console.log(details)
+  const slides = await Slider.find();
   res.render("index",{
     details:details,//now we can fetch it in index.hbs file
+    slides,
   });
+ 
 });
 routes.get("/gallery", async (req, res) => {
   const details = await Detail.findOne({
     "_id":"632063b74fd8ba361252e4fc",
   });
+  const slides = await Slider.find();
   res.render("gallery",{
     details,
+    slides
   });
 });
 module.exports = routes;
