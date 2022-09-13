adaf288f5c7ab13c1ccea908bc1539907f32037b
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-09-13 02:03:00 -0700

    updated

---
+++ src/routes/main.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const routes = express.Router();
+
+routes.get("/", (req, res) => {
+  res.render("index");
+});
+routes.get("/gallery", (req, res) => {
+  res.render("gallery");
+});
+module.exports = routes;
