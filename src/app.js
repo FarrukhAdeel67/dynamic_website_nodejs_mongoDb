73e4413b013567d6783b2df11b01e27ade8ed707
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-09-15 00:12:42 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const hbs = require("hbs");
-const  mongoose = require('mongoose');
-const Detail = require("./models/Detail")
+const mongoose = require("mongoose");
+const Detail = require("./models/Detail");
 const Slider = require("./models/Slider");
 
-const routes = require("./routes/main");//
+const routes = require("./routes/main"); //
 // e.g: static/css/style.css
 app.use("/static", express.static("public"));
 app.use("", routes);
@@ -14,12 +14,15 @@ app.use("", routes);
 //template engine
 app.set("view engine", "hbs");
 app.set("views", "views");
-hbs.registerPartials('views/partials')
+hbs.registerPartials("views/partials");
 
 //db connection
 //addpassword to database
-mongoose.connect("mongodb+srv://farrukhadeel:17301pker@cluster0.ysuiv.mongodb.net/dynamic_website")
-.then(result => {
+mongoose
+  .connect(
+    "mongodb+srv://farrukhadeel:17301pker@cluster0.ysuiv.mongodb.net/dynamic_website"
+  )
+  .then((result) => {
     console.log("Database connected Successfully!");
     // Detail.create({
     //   brandName: "Info Technical Solution",
@@ -67,13 +70,10 @@ mongoose.connect("mongodb+srv://farrukhadeel:17301pker@cluster0.ysuiv.mongodb.ne
     //     imageUrl:"/static/images/slider3.jpg",
     //   },
     // ]);
-    
-
-
-})
-.catch(err => {
-    console.log(err)
-});
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 app.listen(process.env.PORT | port, () => {
   console.log(`sever is running at port ${port}`);
 });
