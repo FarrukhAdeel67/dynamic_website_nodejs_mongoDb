70358b774df33f7e239dbfd71457518039bb4b4b
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-09-15 00:12:50 +0500

    Update Slider.js

--- src/models/Slider.js
+++ src/models/Slider.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
- const Schema = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 const Slider = new Schema({
-    title:String,
-    subTitle:String,
-    imageUrl:String,
-    class:String,
+  title: String,
+  subTitle: String,
+  imageUrl: String,
+  class: String,
 });
 module.exports = mongoose.model("slider", Slider);
